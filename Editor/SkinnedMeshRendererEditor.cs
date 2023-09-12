8977b1698f757e19eb00dc4b973471f96f02c778
Robert Rumney <robertrumney@gmail.com>
2023-09-12 21:56:39 +0200

    Update SkinnedMeshRendererEditor.cs

--- Editor/SkinnedMeshRendererEditor.cs
+++ Editor/SkinnedMeshRendererEditor.cs
@@ -1,30 +1,56 @@
 using UnityEngine;
 using UnityEditor;
+using UnityEditorInternal;
+using System.Collections.Generic;
 
 [CustomEditor(typeof(SkinnedMeshRenderer))]
 public class SkinnedMeshRendererEditor : Editor
 {
+    private ReorderableList bonesList;
+    private static List<Transform> copiedBones;
+
+    private void OnEnable()
+    {
+        bonesList = new ReorderableList(
+            serializedObject, 
+            serializedObject.FindProperty("m_Bones"), 
+            true, true, true, true);
+
+        bonesList.drawHeaderCallback = (Rect rect) => 
+        {
+            EditorGUI.LabelField(rect, "Bones");
+        };
+
+        bonesList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) => 
+        {
+            SerializedProperty element = bonesList.serializedProperty.GetArrayElementAtIndex(index);
+            rect.y += 2;
+            EditorGUI.PropertyField(new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight), 
+                element, GUIContent.none);
+        };
+    }
+
     public override void OnInspectorGUI()
     {
         // Draw the default inspector
         DrawDefaultInspector();
 
-        // Get reference to SkinnedMeshRenderer
-        SkinnedMeshRenderer skinnedMesh = (SkinnedMeshRenderer)target;
+        // Draw the reorderable list
+        bonesList.DoLayoutList();
 
-        // Display bones
-        EditorGUILayout.LabelField("Bones", EditorStyles.boldLabel);
+        // Copy & Paste buttons
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Copy Bones"))
+        {
+            SkinnedMeshRenderer skinnedMesh = (SkinnedMeshRenderer)target;
+            copiedBones = new List<Transform>(skinnedMesh.bones);
+        }
 
-        if (skinnedMesh.bones != null && skinnedMesh.bones.Length > 0)
+        if (GUILayout.Button("Paste Bones") && copiedBones != null)
         {
-            for (int i = 0; i < skinnedMesh.bones.Length; i++)
-            {
-                skinnedMesh.bones[i] = (Transform)EditorGUILayout.ObjectField("Bone " + i, skinnedMesh.bones[i], typeof(Transform), true);
-            }
+            SkinnedMeshRenderer skinnedMesh = (SkinnedMeshRenderer)target;
+            skinnedMesh.bones = copiedBones.ToArray();
         }
-        else
-        {
-            EditorGUILayout.LabelField("No bones assigned.");
-        }
+        EditorGUILayout.EndHorizontal();
     }
 }
